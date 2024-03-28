f316b7ea490b931a1993de57f69149dca923dad7
Alimur Razi <138320899+alimurdipu@users.noreply.github.com>
2024-03-28 18:01:32 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// word vs keyword
+
+// vai
+// bon
+// dipu
+// alimur_razi
+// এইগুলা জাভাস্ক্রিপ্ট চিনে না।
+//.......
+// var
+// const
+// if
+// else
+// for
+// let
+// new
+//এইগুলা জাভাস্ক্রিপ্ট এর রিজার্ভ ওয়ার্ড
+
+
+// variable
+var wife = "sokina"
+var husband = "dipu"
+// var এবং let ব্যবহার করলে ভেল্যু চেঞ্জ হয় 
+
+const wife = "jorina"
+const husband = "dipu"
+// const ব্যবহার করলে ভেল্যু চেঞ্জ হয় না যা প্রথমে আছে তাই থাকে। 
